f555215bf03a2182a5a75e2f57632efb9c4d45b6
zhangkai <zhangkai@xingyoucai.com>
2022-01-10 15:41:07 +0800

    add

--- src/main.js
+++ src/main.js
@@ -39,6 +39,7 @@ axios.defaults.paramsSerializer = function(data){
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
   if(localStorage.getItem("token") !== null) {
+    config.headers.withCredentials = true
     config.headers.Authorization = localStorage.getItem("token")
     config.data['userPhone'] = localStorage.getItem("userPhone")
     config.data['role'] = localStorage.getItem("userRole")
