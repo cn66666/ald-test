8f29817b2661fdcdaf3aba9534ae29f52ab251c6
zhangkai <zhangkai@xingyoucai.com>
2022-05-11 19:25:42 +0800

    add

--- src/view/timer/addJob.vue
+++ src/view/timer/addJob.vue
@@ -30,6 +30,8 @@
           <el-option label="每月开票历史查询" value="invoiceMonthQuery"></el-option>
           <el-option label="历史开票数据补录" value="invoiceQueryErpId"></el-option>
           <el-option label="客户评分卡重置" value="dealerScoreChange"></el-option>
+          <el-option label="检测数据" value="checkInvoiceData"></el-option>
+          <el-option label="变更数据" value="checkInvoiceQuota"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="时间填写" prop="dateTime">
